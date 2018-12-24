d0f615e40d30406363dc4cfd05c057c9b4edccce
Aleix <arlyxs@gmail.com>
2018-12-24 04:10:30 -0400

    added show / hide like function

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ var Movie = Backbone.Model.extend({
   defaults: {
     like: true
   },
-
+  //toggle function
   toggleLike: function() {
     this.set('like', !this.get('like'));
-  }
+  },
+
+  //display liked and up / down images
+
+
+
 
 });
 
@@ -71,17 +76,43 @@ var MovieView = Backbone.View.extend({
 
   initialize: function() {
     // your code here
+    //this.rende();
+    this.model.on('toggle:like', this.render, this);
   },
 
   //bind the event to its handler function
   events: {
+    
     'click button': 'handleClick'
   },
 
   handleClick: function() {
+    //debugger;
     // your code here
-	
-	},
+      if (this.like === true) {
+        this.showLiked();      
+      } else {
+        this.hideLiked();
+      } //if else ends
+    },//display ends
+    
+    //hides Liked 
+    hideLiked: function(e){
+      e = this.attributes.like;
+      e.className = 'hidden';
+      //test for revised data output
+      console.log(e);
+      this.like = true;
+    }, //hides liked ends
+    //shows liked
+    showLiked: function(e){
+      e = this.attributes.like;
+      e.className = 'unhidden';
+      //test for revised data output
+      console.log(e);
+      this.like = false;
+    }, //handle click ends
+
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
