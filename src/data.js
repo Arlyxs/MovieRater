e94e35ebbe40ac0b5eedcad6aba3525ed4334319
Aleix <arlyxs@gmail.com>
2018-12-31 00:01:00 -0400

    unliked to data changed template for liked unliked data, class hide show

--- src/data.js
+++ src/data.js
@@ -3,60 +3,70 @@ var movieData = [
     title: 'Back to the Future',
     year: 1985,
     rating: 10,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Primer',
     year: 2004,
     rating: 9,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Groundhog Day',
     year: 1993,
     rating: 8,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Time Bandits',
     year: 1981,
     rating: 7,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Looper',
     year: 2012,
     rating: 6,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Bill and Ted\'s Excellent Adventure',
     year: 1989,
     rating: 5,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Edge of Tomorrow',
     year: 2014,
     rating: 4,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Hot Tub Time Machine',
     year: 2010,
     rating: 3,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'Safety Not Guaranteed',
     year: 2012,
     rating: 2,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   },
   {
     title: 'The Time Traveler\'s Wife',
     year: 2009,
     rating: 1,
-    liked: 'Liked!'
+    liked: 'Liked!',
+    unliked: 'Unliked'
   }
 ];
